80183f0e24a48078d3382cfa0799aa3a07b73778
Brae Webb <email@braewebb.com>
2022-01-03 20:57:45 +1000

    Organize tasks in the ansible way

--- tests/bin/test.sh
+++ tests/bin/test.sh
@@ -3,33 +3,16 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 # CONTAINER="geerlingguy/docker-ubuntu1804-ansible:latest"
 CONTAINER="ansible"
-PLAYS=(
-    vc/git.yml
-    pkgmng/brew.yml
-    vc/act.yml
-    ansible.yml
-    nodejs.yml
-    isabelle/isabelle.yml
-)
 
 # docker pull "${CONTAINER}"
-# echo
 
 function test_play {
     container=$(docker run --rm -d -v "${REPO_ROOT}":/workdir -w /workdir "${CONTAINER}")
 
-    docker exec --tty "${container}" ansible-playbook $1
-    docker exec --tty "${container}" ansible-playbook $1
+    docker exec --tty "${container}" ansible-playbook "$1" "${@:2}"
+    docker exec --tty "${container}" ansible-playbook "$1" "${@:2}"
 
     docker stop "${container}"
 }
 
-if [ $# -eq 0 ]; then
-    for play in ${PLAYS[@]}; do
-        echo "Testing ${play}"
-        test_play "${play}"
-        echo
-    done
-else
-    test_play "$1"
-fi
+test_play "$1" "${@:2}"
